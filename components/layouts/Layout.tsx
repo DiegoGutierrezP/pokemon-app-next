7e4a5847ea94ee6a963771373bf6bbf1d137dacc
DiegoGutiPlazbot <diego.sk75.dgp@gmail.com>
2023-03-16 23:37:02 -0500

    first commit

---
+++ components/layouts/Layout.tsx
@@ -0,0 +1,39 @@
+import Head from "next/head"
+import { FC, PropsWithChildren } from "react"
+import { Navbar } from '../ui';
+
+interface Props{
+    children:  JSX.Element|JSX.Element[],
+    title?: string
+}
+
+const origin = (typeof window === undefined) ? '': window.location.origin;
+
+export const Layout : FC<Props> = ({children,title}) => {
+
+   
+
+  return (
+    <>
+        <Head>
+            <title>{title || 'Pokemon App'}</title>
+            <meta name="author" content="Diego Gutierrez" />
+            <meta  name="decription" content={`info sobre el pokemon ${title}`} />
+            <meta name="keywords" content="XX, pokemon, pokedex" />
+
+            <meta property="og:title" content={`Informacion sobre ${title}`} />
+            <meta property="og:description" content="Los pokemones de pokemonos." />
+            <meta property="og:image" content={`${origin}/img/banner.png`} />
+        </Head>
+
+        <Navbar/>
+
+        <main style={{
+            padding:'0px 20px'
+        }}>
+            {children}
+        </main>
+
+    </>
+  )
+}
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images:{ //las imagenes que quiero que sean permitidas y next no se queje(mande error)
+    domains:['raw.githubusercontent.com']
+  }
 }
 
 module.exports = nextConfig
